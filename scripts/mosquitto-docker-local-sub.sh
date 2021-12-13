8df3822d9fce60f73c6de62196b55b8b12de1c59
gappc <gapp.christian@gmail.com>
2021-12-13 23:19:53 +0100

    Remove mosquitto related configuration
    The MQTT broker mosquitto is defined as docker-compose
    service and can be started easily with docker-compose. Note
    that this service is started in no-auth mode.
    
    Since this mosquitto instance should be used during dev only,
    it simplifies the project when all mosquitto related config
    is removed.

--- scripts/mosquitto-docker-local-sub.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Subscribing to all topics"
-
-docker run -it --rm --name mosquitto-sub --network="host" eclipse-mosquitto:2.0.12 sh -c "mosquitto_sub -t '#' -v"
--- scripts/mosquitto-docker-sub-to-official-test-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Subscribing to test.mosquitto.org using running docker container named \"mosquitto\""
-
-docker run -it --rm --name mosquitto-sub-test.mosquitto.org eclipse-mosquitto:2.0.12 sh -c "mosquitto_sub -h test.mosquitto.org -t '#' -v"
